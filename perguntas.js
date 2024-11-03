7c165af624c4728c84a551aac4dd80227449528c
Casilu <149337116+Casilu@users.noreply.github.com>
2024-11-03 19:52:36 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'Qual é a origem do CSS?',
+    'A linguagem de marcação generalizada padrão marcou o início das folhas de estilo na década de 1980.'
+)
+
+criaCartao(
+    'Geografia',
+    'Onde fica a cidade de São Petersburgo',
+    'A cidade de São Petersburgo fica na Rússia.'
+)
+
+criaCartao(
+    'Programação',
+    'Que tipos de dados são suportados em JavaScript?',
+    'Número, String, Indeterminado, Nulo e Booleano.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz bom dia em Espanhol?',
+    'Em Espanhol se diz buen día'
+)
